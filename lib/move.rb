cb5fdbb68c8fcfeb50d0f6eef5c8108707613e1f
Leslie Turis <lturis@gmail.com>
2018-08-30 19:35:37 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,17 +8,8 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def move(array, index, value = "X")
-  array[index] = value
-end
-
-board
+def input_to_index(input)
+  input.to_i
 
 
 #Sboard
